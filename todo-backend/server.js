3833cea5b5ffd55a4262e9b6d749ff77b2c9c392
Pournima <pournimasjathade@gmail.com>
2026-02-20 08:31:32 -0700

    first commit

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -10,12 +10,12 @@ app.use(bodyParser.json());
 
 // ✅ MySQL LOCAL connection
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",          // your MySQL user
-  password: "Develop@123",  // your MySQL password
-  database: "todo_app",
-  waitForConnections: true,
-  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+  // waitForConnections: true,
+  // connectionLimit: 10,
 });
 
 (async () => {
